fa754ade3108493ef28bf3284d453f395ad07196
William Pan <wpan@berkeley.edu>
2021-08-01 12:21:43 -0700

    Add description

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: 'We help businesses and professionals communicate more effectively with an international audience.' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
