399f025138e0902ec81056938cd36d4bbbf1872c
hweikel <57724099+hweikel@users.noreply.github.com>
2024-07-25 15:42:53 -0400

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -12,8 +12,8 @@
 <i18n lang="yaml">
 en: 
   home: 
-    header: We help businesses and professionals communicate more effectively with an international audience.
-    subhead: Our expertise covers business writing and editing, presentation coaching, market research for Asian companies, and university admissions counseling for international applicants.
+    header: We help students, researchers, and businesses communicate more effectively with an international audience.
+    subhead: Our expertise covers specialized curriculum design, presentation coaching, advanced copywriting and editing, and university admissions counseling for international applicants.
 kr: 
   home: 
     header: 저희 옥스퍼드 잉글리시 서비스(OES)는 세계 각국의 연구팀, 기관, 기업, 인재 대상으로 전문적인 컨설팅 서비스를 제공합니다.
