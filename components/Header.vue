283987b4e9de94f784d575eb49c94bb9f747410e
Luxi Liu <luxiliu@amazon.co.uk>
2024-12-29 21:08:32 -0800

    fix-header-remove-korean

--- components/Header.vue
+++ components/Header.vue
@@ -4,12 +4,12 @@
       <NuxtLink id="home-link" :to="localePath('/')">Oxford English Services</NuxtLink>
       <div id="small-links">
         <NuxtLink :to="localePath('/about')">{{ t('nav.about') }}</NuxtLink>
-        <NuxtLink :to="localePath('/collegeadmissions')" class="no-wrap" >{{ t('nav.collegeadmissions') }}</NuxtLink>
-        <NuxtLink :to="localePath('/passionprojects')" class="no-wrap" >{{ t('nav.passionprojects') }}</NuxtLink>
-        <NuxtLink :to="localePath('/graduateadmissions')" class="no-wrap">{{ t('nav.graduateadmissions') }}</NuxtLink>
+        <NuxtLink :to="localePath('/collegeadmissions')">{{ t('nav.collegeadmissions') }}</NuxtLink>
+        <NuxtLink :to="localePath('/passionprojects')">{{ t('nav.passionprojects') }}</NuxtLink>
+        <NuxtLink :to="localePath('/graduateadmissions')">{{ t('nav.graduateadmissions') }}</NuxtLink>
         <a href="mailto:contact.oxford.english@gmail.com">{{ t('nav.email') }}</a>
-        <NuxtLink :to="switchLocalePath('kr')" v-if="locale === 'en'">한국어</NuxtLink>
-        <NuxtLink :to="switchLocalePath('en')" v-if="locale === 'kr'">EN</NuxtLink>
+        <!-- <NuxtLink :to="switchLocalePath('kr')" v-if="locale === 'en'">한국어</NuxtLink>
+        <NuxtLink :to="switchLocalePath('en')" v-if="locale === 'kr'">EN</NuxtLink> -->
       </div>
     </nav>
   </header>
@@ -23,54 +23,53 @@ const switchLocalePath = useSwitchLocalePath()
 
 <style scoped>
 header {
-    /* background-color: rgb(0, 33, 71); */ 
-    background-color: white; 
-    padding: 50px var(--margin-sides) 10px; 
-    border-top: 10px solid var(--primary); 
+  /* background-color: rgb(0, 33, 71); */
+  background-color: white;
+  padding: 50px var(--margin-sides) 10px;
+  border-top: 10px solid var(--primary);
 }
 
-header nav { 
-    display: flex; 
-    align-items: baseline; 
-    padding-bottom: 20px; 
-    border-bottom: 1px solid black; 
+header nav {
+  display: flex;
+  align-items: baseline;
+  padding-bottom: 20px;
+  border-bottom: 1px solid black;
 }
 
-header nav a { 
-    color: black; 
-    text-decoration: none; 
-    vertical-align: bottom; 
+header nav a {
+  color: black;
+  text-decoration: none;
+  vertical-align: bottom;
 }
 
-header nav a#home-link { 
-    flex: 1; 
-    font-family: var(--caslon); 
-    font-size: 1.6em; 
+header nav a#home-link {
+  flex: 1;
+  font-family: var(--caslon);
+  font-size: 1.6em;
 }
 
-header nav div#small-links { 
-    flex: 1; 
-    text-align: right; 
+header nav div#small-links {
+  flex: 1;
+  text-align: right;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;  /* This alone will handle spacing between items */
 }
 
-#small-links a + a { 
-    padding-left: 20px; 
-}
-
-.no-wrap {
-  white-space: nowrap; /* Prevents line breaks within the text */
+#small-links a {
+  white-space: nowrap;
 }
 
 @media screen and (max-width: 600px) {
   header nav {
-    flex-direction: column; 
-    text-align: center; 
-    justify-content: center; 
-    align-items: center; 
+    flex-direction: column;
+    text-align: center;
+    justify-content: center;
+    align-items: center;
   }
 
   header nav div#small-links {
-    text-align: center; 
+    text-align: center;
   }
 }
 </style>
