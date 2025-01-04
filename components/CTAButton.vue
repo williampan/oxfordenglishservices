9f6fb005cd9577516d22d3c5f4dda9d367ccafd6
Luxi Liu <luxiliu@amazon.co.uk>
2025-01-04 09:59:28 -0800

    fix-button

--- components/CTAButton.vue
+++ components/CTAButton.vue
@@ -11,39 +11,29 @@
 <style scoped>
 .button-link {
     display: inline-block;
-    /* Allows padding and margin to work properly */
     padding: 10px 20px;
-    /* Add padding for a button-like feel */
     background-color: #002147;
-    /* Button background color */
     color: white;
-    /* Text color */
     text-decoration: none;
-    /* Remove underline */
     border: none;
-    /* Remove border */
     border-radius: 5px;
-    /* Rounded corners */
-    font-size: 32px;
-    /* Adjust font size */
     text-align: center;
-    /* Center the text */
     cursor: pointer;
-    /* Show pointer on hover */
     transition: background-color 0.3s ease;
-    align-items: center; /* Vertical alignment */
-    justify-content: center; /* Horizontal alignment */
+    align-items: center;
+    justify-content: center;
+    width: auto;
+    max-width: 1000px;
+    font-size: clamp(16px, 4vw, 30px);
 }
 
 .button-link:hover {
     background-color: #0056b3;
-    /* Darker shade on hover */
 }
 
 .button-container {
-  display: flex; /* Enables flexbox */
-  justify-content: center; /* Centers the button horizontally */
-  margin-bottom: 10px; /* Optional: Add spacing below the button */
+  display: flex;
+  justify-content: center;
+  margin-bottom: 10px;
 }
-
 </style>
